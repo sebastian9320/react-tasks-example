c04db4a4fbd9ee651e089897eaf2d626bb1769d1
sebastian9320 <sebastianmr320@gmail.com>
2023-01-21 17:33:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import TaskForm from './components/tasks/TaskForm';
+import TaskList from './components/tasks/TaskList';
+
+function App() {
+
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/data/tasks.js
@@ -0,0 +1,27 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primer tarea',
+        description: 'Mi primer tarea'
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'Mi segunda tarea'
+    },
+    {
+        id: 2,
+        title: 'Mi tercer tarea',
+        description: 'Mi tercer tarea'
+    },
+    {
+        id: 3,
+        title: 'Mi cuarta tarea',
+        description: 'Mi cuarta tarea'
+    },
+    {
+        id: 4,
+        title: 'Mi quinta tarea',
+        description: 'Mi quinta tarea'
+    },
+]
